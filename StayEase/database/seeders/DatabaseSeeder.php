fb8b2998a4a9019f8ab430a8f6fbeb2cf1838657
Soufyan <elomranisoufyan@gmail.com>
2026-02-04 17:07:58 +0100

    created aurthentication controllers

--- StayEase/database/seeders/DatabaseSeeder.php
+++ StayEase/database/seeders/DatabaseSeeder.php
@@ -18,8 +18,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Soufyane el omrani',
+            'email' => 'soufyane@example.com',
+            'password' => '1234'
         ]);
     }
 }
