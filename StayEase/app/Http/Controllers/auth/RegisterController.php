39a3c908240f1466b08cb4c67f10ead9ee99505b
Soufyan <elomranisoufyan@gmail.com>
2026-02-04 17:07:58 +0100

    created aurthentication controllers

---
+++ StayEase/app/Http/Controllers/auth/RegisterController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+class RegisterController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        $userData = $request->validate([
+            'name' => ['required','string'],
+            'email' => ['required','email'],
+            'password' => ['required'],
+            'role_id' => 1
+        ]);
+        $userData['password'] = bcrypt($userData['password']);
+        $user = User::create($userData);
+        Auth::login($user);
+        return redirect()->route('home');
+    }
+}
