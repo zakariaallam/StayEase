c8168e5a5dd5bdaa7fbdf3c5d1fcda3e1bc7afba
Soufyan <elomranisoufyan@gmail.com>
2026-02-06 16:56:33 +0100

    fix merge issues

--- StayEase/app/Models/Chambre.php
+++ StayEase/app/Models/Chambre.php
@@ -6,9 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Chambre extends Model
 {
-<<<<<<< HEAD
     protected $fillable = ['numero','description','image','statut','capacite','hotel_id','categorie_id','tag_id','propriete_id'];
-=======
-    //
->>>>>>> 54d174758d4c0a514a971ba0b228b80696bce5db
 }
