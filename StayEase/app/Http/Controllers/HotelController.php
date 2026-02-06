c4f90758b161100f675483ac480c1e11316a1c23
Soufyan <elomranisoufyan@gmail.com>
2026-02-06 16:53:12 +0100

    fix

--- StayEase/app/Http/Controllers/HotelController.php
+++ StayEase/app/Http/Controllers/HotelController.php
@@ -6,7 +6,6 @@ use App\Models\Hotel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
-
 class HotelController extends Controller
 {
     /**
