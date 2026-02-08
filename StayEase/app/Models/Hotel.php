74c0db9e06789c9789b890bef0e27f2318e982ba
Abderrazak <aamaichabderrazak2004@gmail.com>
2026-02-09 00:05:58 +0100

    J'ai modifier sur le galerie des images

--- StayEase/app/Models/Hotel.php
+++ StayEase/app/Models/Hotel.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Hotel extends Model
 {
-    protected $fillable = ['nom','description','adresse','image'];
+    protected $fillable = ['nom', 'description', 'adresse', 'image'];
+    public function chambres()
+    {
+        return $this->hasMany(Chambre::class);
+    }
+    public function images()
+    {
+        return $this->hasMany(Image::class);
+    }
 }
